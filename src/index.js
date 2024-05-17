1c75e21bf97ae2e5874f2ab2f122bf7e16d3565b
turik777 <52492191+turik777@users.noreply.github.com>
2024-05-18 00:44:16 +0300

    Import load functions and add tab-switching logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import homeLoad from "./home";
+import menuLoad from "./menu";
+import contactLoad from "./contact";
 
 homeLoad();
+
+const buttons = document.querySelectorAll("button");
+buttons[2].innerText = "Contact";
+const content = document.querySelector("#content");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        content.innerHTML = "";
+        button.innerText === "Home" ? homeLoad() :
+        button.innerText === "Menu" ? menuLoad() :
+        contactLoad();
+    })
+});
