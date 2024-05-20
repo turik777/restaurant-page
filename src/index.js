0b14ccb88a4a7bce4341aa4fa2f31b49a4db087a
turik777 <52492191+turik777@users.noreply.github.com>
2024-05-21 00:53:38 +0300

    Add function that removes tab color

--- src/index.js
+++ src/index.js
@@ -9,11 +9,19 @@ const buttons = document.querySelectorAll("button");
 buttons[2].innerText = "Contact";
 const content = document.querySelector("#content");
 
+function removeTabColor() {
+    buttons.forEach(button => {
+        button.style.removeProperty("background-color");
+    })
+}
+
 buttons.forEach(button => {
     button.addEventListener("click", () => {
+        removeTabColor();
         content.innerHTML = "";
         button.innerText.toLowerCase() === "home" ? homeLoad() :
         button.innerText.toLowerCase() === "menu" ? menuLoad() :
         contactLoad();
+        button.style.backgroundColor = "#F2EFE3";
     })
 });
