d08528ecfcc2230f6ce1197b5bf7ed0dc343bda0
turik777 <52492191+turik777@users.noreply.github.com>
2024-05-20 22:20:36 +0300

    Shorten text

--- src/home.js
+++ src/home.js
@@ -7,7 +7,7 @@ export default function homeLoad() {
     const text = document.createElement("p");
 
     headline.innerText = "Principal Restaurant"
-    text.innerText = `Welcome to Principal Restaurant, where culinary excellence meets timeless elegance. Nestled in the heart of the city, Principal Restaurant offers a sophisticated dining experience that blends modern innovation with classic flavors. Our menu features a curated selection of gourmet dishes, crafted with the freshest locally-sourced ingredients, celebrating the best of seasonal cuisine. \n\n Step into our warm and inviting ambiance, where contemporary design meets a touch of vintage charm. Whether you’re joining us for a business lunch, a romantic dinner, or a special celebration, our attentive and knowledgeable staff will ensure your visit is unforgettable. At Principal Restaurant, we are dedicated to providing impeccable service and creating a memorable culinary journey for every guest. \n\n Discover the art of dining at its finest at Principal Restaurant – where every meal is a masterpiece.`;
+    text.innerText = `Welcome to Principal Restaurant, where culinary excellence meets timeless elegance.`;
     image.src = "/img/restaurant.jpg";
     image.width = 1000;
 
